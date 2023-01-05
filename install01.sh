49ece9aea462130c67ac972efe83b61d14cad3bc
Th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-01-05 11:40:31 +0530

    Update install01.sh

--- install01.sh
+++ install01.sh
@@ -41,9 +41,19 @@ echo "Installation completed"
 sleep 0.5
 clear
 sleep 0.5
+echo "generating fstab"
+sleep 0.5
 cd 
 genfstab -U /mnt >> /mnt/etc/fstab
 sleep 0.5
-
+mkdir /mnt/cai
+touch /mnt/cai/install02.sh
+pacman -S --noconfirm --needed curl
+curl -o /mnt/cai/install02.sh
+clear
+echo "Installation part 1 completed"
+echo "run install02.sh in cai folder"
+sleep 0.5
+arch-chroot /mnt
 exit
 
